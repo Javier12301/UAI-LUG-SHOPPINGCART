caa6bfecfabd7055e529c2b182bfb0ddb2aa8bc9
Javier12301 <javierramirez1230123@gmail.com>
2022-10-31 17:39:24 -0300

    Update

--- Controllers/Productos.ts
+++ Controllers/Productos.ts
@@ -23,8 +23,15 @@ const productosController = {
     getunique: async (req: Request, res: Response) => {
         try
         {
-            const verProductosUnique = await productosModel.findOne({...req.params})
-            res.status(200).send(verProductosUnique)
+            const verProductosUnique = await productosModel.findOne({... req.params})
+            
+            //Sí el producto no existe la API mandará un HTTP STATUS NOT FOUND
+            if(verProductosUnique?.Nombre_Producto != undefined)
+            {
+                res.status(200).send(verProductosUnique)
+            }else{
+                res.status(404).send(`El producto escrito en los parametros no existe en la base de datos.`);
+            }
         }
         catch (error)
         {
@@ -42,6 +49,14 @@ const productosController = {
         } catch (error) {
             //Los valos de status son los tipos de errores que nosotros queremos que salga -> 500 es error de servidor
             res.status(500).send(error)
+        }
+    },
+
+    //Para eliminar los productos
+    delete: async (req: Request, res: Response) => {
+        try
+        {
+
         }
     },
 
--- routes/api/Productos.ts
+++ routes/api/Productos.ts
@@ -6,7 +6,7 @@ const router = Router();
 //Aquí utilizaremos las controladoras creadas para productos.
 router.get("/", productosController.get);
 //Utilizar parametros para seleccionar un producto único
-router.get('/:id', productosController.getunique);
+router.get('/:Nombre_Producto', productosController.getunique);
 router.post("/", productosController.add);
 
 export default router;
